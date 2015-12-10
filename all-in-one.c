430e90a81e61fefee5debc76ca1045481086c509
David Sterba <dave@jikos.cz>
2015-12-11 00:25:46 +0100

    all-in-one: add stub plugin
    Combined functionality provided by one plugin.
    
    Signed-off-by: David Sterba <dave@jikos.cz>

---
+++ all-in-one.c
@@ -0,0 +1,36 @@
+/*
+ * Combined functionality into one plugin
+ */
+
+#define MERGED
+
+#include <dnscrypt/plugin.h>
+#include "common.h"
+
+DCPLUGIN_MAIN(__FILE__);
+
+const char *dcplugin_description(DCPlugin *const dcplugin)
+{
+	return "All-in-one plugin: validate, empty-aaaa, logger, blacklist";
+}
+
+const char *dcplugin_long_description(DCPlugin *const dcplugin)
+{
+	return
+	    "All-in-one plugin: validate, empty-aaaa, logger, blacklist";
+	    "\n"
+	    "blah\n"
+	    "\n"
+	    "# dnscrypt-proxy --plugin=libdcplugin_all_in_one.so";
+}
+
+int dcplugin_init(DCPlugin *const dcplugin, int argc, char *argv[])
+{
+	return 0;
+}
+
+int dcplugin_destroy(DCPlugin *const dcplugin)
+{
+	return 0;
+}
+
